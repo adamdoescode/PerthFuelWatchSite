38dc0ea161636824b4199800780e53a110066870
Adamdoescode <13943324+adamdoescode@users.noreply.github.com>
2023-04-03 18:45:19 +0800

    import more recent files from orphaned more recent repo

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
+import backend
 from flask import Flask, render_template, request
 app = Flask(__name__)
-import backend
 
 @app.route('/')
 def index():
@@ -18,7 +18,26 @@ def getData():
     print(f'Here is the request args: {request.args}')
     print(f'Here is the data: {str(data)[:50]}')
     print(f'Here is the HTML table: {str(fuelTableHTML)[:50]}')
-    return render_template('index.html', fuelDataTable=fuelTableHTML)
+    print(
+        f'''these are the paths to the graphs:
+
+        '''
+    )
+    return render_template(
+        'index.html', 
+        fuelDataTable=fuelTableHTML,
+        data_date=backend.TODAYS_DATE
+    )
+
+@app.route('/results/plots')
+def getPlots(whichPlot: str):
+    '''
+    Returns the data from the fuelwatch RSS feed
+    Using get_fuel function
+    '''
+    print(f'I got clicked!')
+    data = backend.retrieveData()
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
